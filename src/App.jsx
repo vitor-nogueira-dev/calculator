cdc36ea399ea1c7972600473ba3a0df12c7ec3f5
Vitor Nogueira <vleonel96@gmail.com>
2025-01-14 06:54:46 -0300

    refactor: remove title from the calculator app

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="app-title">
-          Calculator
-        </h1>
         <Calculator />
       </div>
     )
